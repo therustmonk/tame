ce208a76516e0d2a454c4e899ef2283778891f59
Denis Kolodin <deniskolodin@gmail.com>
2025-04-09 21:45:40 +0200

    Use the registrar in the Pub new method

--- tame/src/pubsub/publisher/mod.rs
+++ tame/src/pubsub/publisher/mod.rs
@@ -1,21 +1,38 @@
-mod agent;
+pub mod agent;
 
-use super::PubSub;
+use crate::pubsub::PubSub;
+use crate::pubsub::registrar::Registrar;
 use agent::PubAgent;
 use crb::agent::Address;
+use derive_more::{Deref, DerefMut};
 use std::sync::Arc;
 
+#[derive(Deref, DerefMut)]
 pub struct Pub<T: PubSub> {
     inner: Arc<T::Publisher>,
+}
+
+impl<T: PubSub> Clone for Pub<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
 }
 
 impl<T: PubSub> Pub<T> {
     pub fn new(state: T) -> Self {
         let agent = PubAgent::new(state);
-        todo!()
+        let registrar = Registrar::link();
+        let agent = registrar.spawn_pub(agent);
+        let pub_inner = PubInner { agent };
+        let publisher = T::Publisher::from(pub_inner);
+        let inner = Arc::new(publisher);
+        Self { inner }
     }
 }
 
+#[derive(Deref, DerefMut)]
 pub struct PubInner<T: PubSub> {
     agent: Address<PubAgent<T>>,
 }
--- tame/src/pubsub/registrar.rs
+++ tame/src/pubsub/registrar.rs
@@ -1,7 +1,32 @@
-use crb::agent::{Agent, AgentSession};
+use crate::pubsub::PubSub;
+use crate::pubsub::publisher::agent::PubAgent;
+use crb::agent::{Address, Agent, AgentSession, RunAgent};
+use crb::runtime::InteractiveRuntime;
 use crb::superagent::{Supervisor, SupervisorSession};
+use std::sync::OnceLock;
+
+static REGISTRAR: OnceLock<RegistrarLink> = OnceLock::new();
+
+pub struct RegistrarLink {
+    address: Address<Registrar>,
+}
+
+impl RegistrarLink {
+    pub fn spawn_pub<T: PubSub>(&self, agent: PubAgent<T>) -> Address<PubAgent<T>> {
+        let runtime = RunAgent::new(agent);
+        let address = runtime.address();
+        // TODO: Spanw the runtime in the registrar
+        address
+    }
+}
 
 pub struct Registrar {}
+
+impl Registrar {
+    pub fn link() -> &'static RegistrarLink {
+        REGISTRAR.get().unwrap()
+    }
+}
 
 impl Supervisor for Registrar {
     type BasedOn = AgentSession<Self>;
