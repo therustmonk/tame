2e20d7d71b223c802262de1ae529dbeb8241bbe0
Denis Kolodin <deniskolodin@gmail.com>
2025-04-12 16:45:06 +0200

    Spawn a runtime

--- tame/src/pubsub/registrar.rs
+++ tame/src/pubsub/registrar.rs
@@ -1,11 +1,16 @@
 use crate::pubsub::PubSub;
 use crate::pubsub::publisher::agent::PubAgent;
-use crb::agent::{Address, Agent, AgentSession, RunAgent};
-use crb::runtime::InteractiveRuntime;
-use crb::superagent::{Supervisor, SupervisorSession};
+use anyhow::Result;
+use async_trait::async_trait;
+use crb::agent::{Address, Agent, Context, DoAsync, Next, OnEvent, RunAgent};
+use crb::runtime::{InteractiveRuntime, Runtime};
+use crb::superagent::{EventBridge, StreamSession, Supervisor, SupervisorSession};
+use std::sync::LazyLock;
 use std::sync::OnceLock;
 
 static REGISTRAR: OnceLock<RegistrarLink> = OnceLock::new();
+
+static BRIDGE: LazyLock<EventBridge<Control>> = LazyLock::new(|| EventBridge::new());
 
 pub struct RegistrarLink {
     address: Address<Registrar>,
@@ -15,7 +20,11 @@ impl RegistrarLink {
     pub fn spawn_pub<T: PubSub>(&self, agent: PubAgent<T>) -> Address<PubAgent<T>> {
         let runtime = RunAgent::new(agent);
         let address = runtime.address();
-        // TODO: Spanw the runtime in the registrar
+        let control = Control {
+            runtime: Box::new(runtime),
+            group: Group::Publisher,
+        };
+        BRIDGE.send(control);
         address
     }
 }
@@ -28,11 +37,45 @@ impl Registrar {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Group {
+    Publisher,
+    Subscriber,
+}
+
 impl Supervisor for Registrar {
-    type BasedOn = AgentSession<Self>;
-    type GroupBy = ();
+    type BasedOn = StreamSession<Self>;
+    type GroupBy = Group;
 }
 
 impl Agent for Registrar {
     type Context = SupervisorSession<Self>;
+
+    fn begin(&mut self) -> Next<Self> {
+        Next::do_async(Initialize)
+    }
+}
+
+struct Initialize;
+
+#[async_trait]
+impl DoAsync<Initialize> for Registrar {
+    async fn handle(&mut self, _: Initialize, ctx: &mut Context<Self>) -> Result<Next<Self>> {
+        let events = BRIDGE.events().await?;
+        ctx.consume(events);
+        Ok(Next::events())
+    }
+}
+
+struct Control {
+    runtime: Box<dyn Runtime>,
+    group: Group,
+}
+
+#[async_trait]
+impl OnEvent<Control> for Registrar {
+    async fn handle(&mut self, event: Control, ctx: &mut Context<Self>) -> Result<()> {
+        let rel = ctx.spawn_trackable(event.runtime, event.group);
+        Ok(())
+    }
 }
